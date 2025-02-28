3dbb0eecb9d22e99c7047d10a693833e805fa12c
Reid <reid.al.harrisson@gmail.com>
2025-02-28 19:28:59 +0100

    feat: add ui components and configuration for css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailWindCSS from "@tailwindcss/vite";
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailWindCSS()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
